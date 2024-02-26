2e4d0828dd7c61a0562c144c9e808389d801b53d
kimys5278 <kimys5278@gmail.com>
2024-02-26 13:42:50 +0900

    백엔드초기설정

---
+++ src/main/java/com/springboot/yogijogi/YogijogiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.yogijogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogijogiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogijogiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/yogijogi/YogijogiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.yogijogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogijogiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
